ab879d5ee58f41542f4a63388ac73c574a1b6de0
Floyd Hightower <floyd.hightower27@gmail.com>
2018-01-29 09:54:58 -0500

    Improving tests

--- tests/test_http_request_playbook_creation_ui.py
+++ tests/test_http_request_playbook_creation_ui.py
@@ -2,8 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import html
 
 from http_request_playbook_creation_ui import http_request_playbook_creation_ui
+
+
+def check_heading(response):
+    """Make sure the heading is shown in a response."""
+    assert 'HTTP Request Playbook Creation UI' in response
+    assert 'UI for creating http request playbook.' in response
+
+
+def check_index(response):
+    """Make sure the response contains everything that should be in the index."""
+    check_heading(response)
+    assert "Try 'https://ioc-fang.github.io/datasets/fang.json'" in response
 
 
 class HttpRequestPlaybookCreationUiTestCase(unittest.TestCase):
@@ -13,5 +26,30 @@ class HttpRequestPlaybookCreationUiTestCase(unittest.TestCase):
 
     def test_get_index(self):
         rv = self.app.get('/')
-        self.assertIn('HTTP Request Playbook Creation UI', rv.data.decode())
-        self.assertIn('UI for creating http request playbook.', rv.data.decode())
+        check_index(rv.data.decode())
+
+    def test_json_parse(self):
+        rv = self.app.get('/json?url=https%3A%2F%2Fioc-fang.github.io%2Fdatasets%2Ffang.json')
+        check_heading(rv.data.decode())
+        assert '[.]' in rv.data.decode()
+        assert 'xxxxx://' in rv.data.decode()
+        assert 'Name of playbook output variable:' in rv.data.decode()
+
+    def test_empty_json_parse(self):
+        """Make sure an error message is shown if no content is given to the json path."""
+        rv = self.app.get('/json', follow_redirects=True)
+        check_index(rv.data.decode())
+        assert "Please enter a URL or some json before continuing." in rv.data.decode()
+
+    def test_blank_pb_creation(self):
+        """Make sure that the blank playbook template created by this app is correct."""
+        rv = self.app.post('/pb', data={
+            'jsonPaths': '[]',
+            'url': ''
+        })
+        check_heading(rv.data.decode())
+        assert 'Complete Playbook' in rv.data.decode()
+        assert 'Partial Playbook' in rv.data.decode()
+        assert '&#34;definitionVersion&#34; : &#34;1.0.0&#34;,' in rv.data.decode()
+
+    # TODO: Add more tests for the pb creation step with specific values
