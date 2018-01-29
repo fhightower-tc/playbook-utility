94c90e2f51e7c63278c3b38b25d36d3c0c470df4
Floyd Hightower <floyd.hightower27@gmail.com>
2018-01-29 09:55:21 -0500

    Improving formatting and adding flash messages for errors

--- http_request_playbook_creation_ui/http_request_playbook_creation_ui.py
+++ http_request_playbook_creation_ui/http_request_playbook_creation_ui.py
@@ -34,110 +34,110 @@ def parse_json():
         if r.ok:
             return render_template("json.html", responseData=r.text, url=request.args['url'])
         else:
-            print("Error requesting {}: {}".format(request.args['url'], r.text))
-            # TODO: raise error message here
+            flash('Got a {} response code from {}. Please try another site or copy the json and paste it into the text area below.'.format(r.status_code, request.args['url']), 'error')
             return redirect(url_for('index'))
     elif request.args.get('json'):
         return render_template("json.html", responseData=request.args['json'], url='N/A')
     else:
-        # TODO: raise error message here
+        flash('Please enter a URL or some json before continuing.', 'error')
         return redirect(url_for('index'))
 
 
 @app.route("/pb", methods=['POST'])
 def create_playbook():
     partial_pb_template = """ "jobList": [{
-        "id": 1,
-        "appCatalogItem": {
-            "programName": "Http Client",
-            "displayName": "HTTP Client",
-            "programVersion": "1.0.0"
+    "id": 1,
+    "appCatalogItem": {
+        "programName": "Http Client",
+        "displayName": "HTTP Client",
+        "programVersion": "1.0.0"
+    },
+    "name": "HTTP Client 1",
+    "jobParameterList": [{
+        "appCatalogItemParameter": {
+            "paramName": "headers"
         },
-        "name": "HTTP Client 1",
-        "jobParameterList": [{
-            "appCatalogItemParameter": {
-                "paramName": "headers"
-            },
-            "value": "[]"
-        }, {
-            "appCatalogItemParameter": {
-                "paramName": "url"
-            },
-            "value": "%s"
-        }, {
-            "appCatalogItemParameter": {
-                "paramName": "httpclient_proxy"
-            },
-            "value": "false"
-        }, {
-            "appCatalogItemParameter": {
-                "paramName": "action"
-            },
-            "value": "GET"
-        }, {
-            "appCatalogItemParameter": {
-                "paramName": "parameters"
-            },
-            "value": "[]"
-        }, {
-            "appCatalogItemParameter": {
-                "paramName": "ignore_ssl_trust"
-            },
-            "value": "false"
-        }, {
-            "appCatalogItemParameter": {
-                "paramName": "body"
-            }
-        }],
-        "locationLeft": -1080.0,
-        "locationTop": 170.0,
-        "playbookRetryEnabled": false
+        "value": "[]"
     }, {
-        "id": 2,
-        "appCatalogItem": {
-            "programName": "TCPB - JsonPath v1.0",
-            "displayName": "Json Path",
-            "programVersion": "2.0.1"
+        "appCatalogItemParameter": {
+            "paramName": "url"
         },
-        "name": "Json Path 1",
-        "jobParameterList": [{
-            "appCatalogItemParameter": {
-                "paramName": "column_mapping"
-            },
-            "value" : "%s"
-        }, {
-            "appCatalogItemParameter": {
-                "paramName": "json_content"
-            },
-            "value": "#App:1:http_client.response.output_content!String"
-        }, {
-            "appCatalogItemParameter": {
-                "paramName": "null_missing_leaf"
-            },
-            "value": "false"
-        }],
-        "locationLeft": -740.0,
-        "locationTop": 260.0,
-        "playbookRetryEnabled": false
+        "value": "%s"
+    }, {
+        "appCatalogItemParameter": {
+            "paramName": "httpclient_proxy"
+        },
+        "value": "false"
+    }, {
+        "appCatalogItemParameter": {
+            "paramName": "action"
+        },
+        "value": "GET"
+    }, {
+        "appCatalogItemParameter": {
+            "paramName": "parameters"
+        },
+        "value": "[]"
+    }, {
+        "appCatalogItemParameter": {
+            "paramName": "ignore_ssl_trust"
+        },
+        "value": "false"
+    }, {
+        "appCatalogItemParameter": {
+            "paramName": "body"
+        }
     }],
-    "playbookConnectionList": [{
-        "type": "Pass",
-        "isCircularOnTarget": false,
-        "sourceJobId": 1,
-        "targetJobId": 2
-    }],"""
+    "locationLeft": -1080.0,
+    "locationTop": 170.0,
+    "playbookRetryEnabled": false
+}, {
+    "id": 2,
+    "appCatalogItem": {
+        "programName": "TCPB - JsonPath v1.0",
+        "displayName": "Json Path",
+        "programVersion": "2.0.1"
+    },
+    "name": "Json Path 1",
+    "jobParameterList": [{
+        "appCatalogItemParameter": {
+            "paramName": "column_mapping"
+        },
+        "value" : "%s"
+    }, {
+        "appCatalogItemParameter": {
+            "paramName": "json_content"
+        },
+        "value": "#App:1:http_client.response.output_content!String"
+    }, {
+        "appCatalogItemParameter": {
+            "paramName": "null_missing_leaf"
+        },
+        "value": "false"
+    }],
+    "locationLeft": -740.0,
+    "locationTop": 260.0,
+    "playbookRetryEnabled": false
+}],
+"playbookConnectionList": [{
+    "type": "Pass",
+    "isCircularOnTarget": false,
+    "sourceJobId": 1,
+    "targetJobId": 2
+}],"""
 
-    full_pb_template = """ {
-        "definitionVersion" : "1.0.0",
-        "name" : "%s Requester Playbook",
-        "panX" : 1392.0,
-        "panY" : 20.0,
-        "logLevel" : "WARN",
-        "description" : "Playbook that makes requests to %s and parses the json.",
-        %s
-        "playbookTriggerList" : [ ],
-        "dateExported" : "1/12/18 7:17 PM"
-    }"""
+    full_pb_template = """
+{
+    "definitionVersion" : "1.0.0",
+    "name" : "%s Requester Playbook",
+    "panX" : 1392.0,
+    "panY" : 20.0,
+    "logLevel" : "WARN",
+    "description" : "Playbook that makes requests to %s and parses the json.",
+    %s
+    "playbookTriggerList" : [ ],
+    "dateExported" : "1/12/18 7:17 PM"
+}"""
 
     json_paths = json.loads(request.form['jsonPaths'])
     reformatted_json = [{"key": path['name'], "value": path['path']} for path in json_paths]
